9f94272c003916bddb5d51e539dc3d61b0d64a0e
bibekdhkl <bibekdh2016@gmail.com>
2022-01-04 01:59:58 +0545

    Day02 missed problem

---
+++ day02-Arrays/2DArrays.cpp
@@ -0,0 +1,54 @@
+//Given an 2-d array ofintegers havingN*Nelements, print the transpose of the array.
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define M 101
+
+vector<vector<int>> transpose(int a[][M], int n);
+
+int main()
+{
+    int t;
+    cin>>t;
+    int a[M][M];
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>a[i][j];
+            }
+        }
+        vector<vector<int>> b;
+        b = transpose(a, n);
+        for(int i=0;i < n;i++)
+        {
+            for(int j=0;j<n;j++){
+                cout << b[i][j] << " ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+
+// } Driver Code Ends
+
+
+vector<vector<int>> transpose(int a[][M], int n)
+{
+    // Code here
+	// This code is not working
+    vector<vector<int>> v(n,vector<int>(n));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+           v[i][j]=a[j][i];
+        }
+    }
+   
+    return v;
+}
+
